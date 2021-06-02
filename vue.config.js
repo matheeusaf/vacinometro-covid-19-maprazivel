82f3a95ab8c98f64ec991d2348a869db4e56ca85
Matheus A. Fernandes <mattheus.afernandes@gmail.com>
2021-06-02 09:47:24 -0300

    Fix icon paths and added apple config

--- vue.config.js
+++ vue.config.js
@@ -3,80 +3,82 @@ module.exports = {
     'vuetify'
   ],
   pwa: {
+    name: "Vacinômetro Covid-19 Monte Aprazível",
     themeColor: "#fafafa",
     msTileColor: "#e57373",
-    name: "Vacinômetro Covid-19 Monte Aprazível",
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'white',
     manifestOptions: {
       icons: [
         {
-          src: "src/assets/img/android-chrome-192x192.png",
+          src: "img/icons/android-chrome-192x192.png",
           sizes: "192x192",
           type: "image/png",
         },
         {
-          src: "src/assets/img/android-chrome-512x512.png",
+          src: "img/icons/android-chrome-512x512.png",
           sizes: "512x512",
           type: "image/png",
         },
         {
-          src: "src/assets/img/android-chrome-maskable-192x192.png",
+          src: "img/icons/android-chrome-maskable-192x192.png",
           sizes: "192x192",
           type: "image/png",
           purpose: "maskable",
         },
         {
-          src: "src/assets/img/android-chrome-maskable-512x512.png",
+          src: "img/icons/android-chrome-maskable-512x512.png",
           sizes: "512x512",
           type: "image/png",
           purpose: "maskable",
         },
         {
-          src: "src/assets/img/apple-touch-icon-60x60.png",
+          src: "img/icons/apple-touch-icon-60x60.png",
           sizes: "60x60",
           type: "image/png",
         },
         {
-          src: "src/assets/img/apple-touch-icon-76x76.png",
+          src: "img/icons/apple-touch-icon-76x76.png",
           sizes: "76x76",
           type: "image/png",
         },
         {
-          src: "src/assets/img/apple-touch-icon-120x120.png",
+          src: "img/icons/apple-touch-icon-120x120.png",
           sizes: "120x120",
           type: "image/png",
         },
         {
-          src: "src/assets/img/apple-touch-icon-152x152.png",
+          src: "img/icons/apple-touch-icon-152x152.png",
           sizes: "152x152",
           type: "image/png",
         },
         {
-          src: "src/assets/img/apple-touch-icon-180x180.png",
+          src: "img/icons/apple-touch-icon-180x180.png",
           sizes: "180x180",
           type: "image/png",
         },
         {
-          src: "src/assets/img/apple-touch-icon.png",
+          src: "img/icons/apple-touch-icon.png",
           sizes: "180x180",
           type: "image/png",
         },
         {
-          src: "src/assets/img/favicon-16x16.png",
+          src: "img/icons/favicon-16x16.png",
           sizes: "16x16",
           type: "image/png",
         },
         {
-          src: "src/assets/img/favicon-32x32.png",
+          src: "img/icons/favicon-32x32.png",
           sizes: "32x32",
           type: "image/png",
         },
         {
-          src: "src/assets/img/msapplication-icon-144x144.png",
+          src: "img/icons/msapplication-icon-144x144.png",
           sizes: "144x144",
           type: "image/png",
         },
         {
-          src: "src/assets/img/mstile-150x150.png",
+          src: "img/icons/mstile-150x150.png",
           sizes: "150x150",
           type: "image/png",
         },
