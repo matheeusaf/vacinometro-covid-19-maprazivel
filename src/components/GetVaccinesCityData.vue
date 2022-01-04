7236802d14e29a0047793a4d27271c45f9b04643
matheeusaf <mattheus.afernandes@gmail.com>
2022-01-04 20:08:53 -0300

    Moved API Key to env

--- src/components/GetVaccinesCityData.vue
+++ src/components/GetVaccinesCityData.vue
@@ -143,7 +143,7 @@
       var data = new Date();
       var myHeaders = new Headers();
       myHeaders.append("origin", "same-origin");
-      myHeaders.append("x-cors-grida-api-key", "89566613-cb0b-423b-a6a2-46695353a8c6");
+      myHeaders.append("x-cors-grida-api-key", process.env.VUE_APP_CORS_KEY);
       var requestOptions = {
         method: 'GET',
         headers: myHeaders,
